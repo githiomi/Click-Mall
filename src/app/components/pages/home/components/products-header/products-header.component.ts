0667a64ed9d4a258f1831bc509575762841d12d5
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-10 22:18:42 +0400

    Fully implemented the sort and product counter

--- src/app/components/pages/home/components/products-header/products-header.component.ts
+++ src/app/components/pages/home/components/products-header/products-header.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class ProductsHeaderComponent {
 
+  // To keep track of the sort order
+  sortOrder: string = 'desc';
+
+  // To keep track of the total number of products
+  totalProducts: number = 12;
+
+  constructor() { }
+
+  // Method to update the sort order
+  updateSortOrder(newOrder: string): void {
+    this.sortOrder = newOrder;
+  }
+
+  // Method to update the total number of items
+  updateTotalItemsCounter(newItemCounter: number): void {
+    this.totalProducts = newItemCounter;
+  }
+
+
 }
