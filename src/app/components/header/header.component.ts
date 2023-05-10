dfb26299671450f6977e5685c52725fe992bf6e4
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-10 20:36:50 +0400

    Completed the cart list dropdown menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+// Interface import
+import { Product } from '../../models/Product';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  product: Product = {
+    itemName: "Keyboard",
+    itemCounter: 2,
+    itemPrice: 10.57
+  }
+
+  // Method call to get cart total
+  getCartTotal() {
+    return 10.57 * 2;
+  }
+
+  // Method call to clear cart
+  clearCart() {
+    confirm("Are you sure you want to clear the cart?");
+  }
+
+  // Method to checkout the cart
+  checkoutCart() {
+    confirm("Are you sure you want to clear the cart?");
+  }
 
 }
---
+++ src/app/models/Product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+
+    itemName : string,
+    itemCounter: number,
+    itemPrice: number
+
+}
