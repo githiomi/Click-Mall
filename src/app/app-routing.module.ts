62844373ac12e04d411e4bdb4a86f12e5b4b2ea8
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-10 20:45:17 +0400

    Added app routing and added the router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components imported for routing
+import { HomeComponent } from './components/pages/home/home.component';
+
+const routes: Routes = [
+  // Application Routes
+
+  {
+    // Home route
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    // Setting the default route for rerouting
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
