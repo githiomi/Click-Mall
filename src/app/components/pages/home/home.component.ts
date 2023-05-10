b66693cecbb0201560287275d35dd19bc6e8cbcf
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-10 22:36:17 +0400

    Emits the number of columns to change layout

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  // To keep track of the number of products per row
+  productColumns : number = 3;
+
+  constructor(){
+
+  }
+
+  // Method that will be triggered when the user clicks any change layout icon
+  changeColumnsLayout(newProductColumns:number){
+    console.log(`Changed Columns Layout number to ${newProductColumns}`);
+    this.productColumns = newProductColumns;
+  }
+
 }
