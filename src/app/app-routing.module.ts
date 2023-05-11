743e74591b266b58b49a926a4721166e17e527a7
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-11 12:39:39 +0400

    Configured cart routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Components imported for routing
 import { HomeComponent } from './components/pages/home/home.component';
+import { CartComponent } from './components/pages/cart/cart.component';
 
 const routes: Routes = [
   // Application Routes
@@ -11,6 +12,11 @@ const routes: Routes = [
     // Home route
     path: 'home',
     component: HomeComponent
+  },
+  {
+    // Cart route
+    path: 'cart',
+    component: CartComponent
   },
   {
     // Setting the default route for rerouting
