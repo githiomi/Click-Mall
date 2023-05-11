322802a770d8a2c7a1b7779725fbad99ae02e581
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-11 14:11:13 +0400

    Created the cart mdoel class interfaces

---
+++ src/app/models/Cart-Item.ts
@@ -0,0 +1,9 @@
+export interface CartItem {
+
+    product: string,
+    name: string,
+    price: number,
+    quantity: number,
+    id: number
+
+}
---
+++ src/app/models/Cart.ts
@@ -0,0 +1,8 @@
+// Import the cart item interface
+import {CartItem} from './Cart-Item';
+
+export interface Cart {
+
+    cartItems : Array<CartItem>;
+
+}
