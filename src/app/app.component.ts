34dd5be94a5b8c8865eab462c730b495c249381e
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-15 02:48:19 +0400

    Used the service to update cart header on item add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Interface imports
+import { Cart } from './models/Cart';
+
+// Service imports
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Click-Mall';
+
+export class AppComponent implements OnInit {
+  
+  // This is to pass the cart to the 'header' component
+  cart : Cart = {
+    cartItems : [] // Init the cart to an empty array
+  }
+
+  constructor(
+    // Inject the cart service
+    private _cartService: CartService
+  ){}
+
+  ngOnInit(): void {
+
+    // When the component is initialized, initialize the service and subscribe to it
+    this._cartService.cart.subscribe(
+      ( _cart ) => {
+        // It returns a cart object which we assign locally
+        this.cart = _cart;
+      }
+    )
+    
+  }
+  
 }
