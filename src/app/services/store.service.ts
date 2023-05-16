4d22937c5392334d926eb32901175bd6bf77e944
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-16 11:40:49 +0400

    Created the store service to replace the products

---
+++ src/app/services/store.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+
+// The FAKE STORE API base URL
+const FAKESTOREAPI_BASE_URL = 'http://fakestoreapi.com';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// This service is responsible for any API calls to the fakestore api
+export class StoreService {
+
+  
+
+  constructor() { }
+}
